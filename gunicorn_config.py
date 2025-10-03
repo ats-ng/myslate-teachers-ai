4cccf7367f76b5f8c783851e344c39de8ea92c3c
Danjuma1 <hda0202@gmail.com>
2025-10-03 12:22:47 +0100

    edited gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,13 @@
-# gunicorn_config.py
+# gunicorn_config.py (minimal version)
 import multiprocessing
 
-bind = "0.0.0.0:8000"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 max_requests = 1000
 max_requests_jitter = 100
 timeout = 120
-keepalive = 5
+keepalive = 2
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
